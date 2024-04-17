ce767f1cf86beec9c8d47363b7d67afae9ecad3c
Sheyifunmi4 <155637789+Sheyifunmi4@users.noreply.github.com>
2024-04-17 15:09:33 +0100

    Add files via upload

---
+++ Functional Calculator/home.js
@@ -0,0 +1,76 @@
+// let firstname= "Seyi";
+// let lastname= "Adebayo";
+// let fullname= firstname + " " +lastname;
+// console.log(fullname);
+
+// let num1=2;
+// let num2=4;
+// let result= num1 + num2;
+// console.log(result);  
+
+//toUppercase
+
+// let text1= "hello handsome"
+// let text2= text1.toUpperCase();
+// console.log(text2);
+
+//repeat
+// let text1=" i'm hungry";
+// let text2= text1.repeat(4);
+// console.log(text2);
+
+//replace
+// let text1= "i'm hungry"
+// let text2= text1.replace(text1, "still hungry")
+// console.log(text2);
+
+//Math Method
+// let number = 4.55;
+// let mathround = Math.round(number);
+// console.log(mathround);
+
+//Math Floor
+// let number= 7.25367;
+// let floorNumber= Math.floor(number);
+// console.log(floorNumber);
+
+//Math.ceil
+// let number= 8.25367
+// let floorNumber= Math.ceil(number);
+// console.log(floorNumber)
+
+//conditional statement
+// let votersAge = 18;
+// let eligible = "Go cast your vote";
+// let notEligible="Go home you are not eligible";
+
+
+// if (votersAge) >= 18 {
+//     console.log(eligible)
+// }
+// else{
+//     console.log(notEigible)
+// }
+
+// let accountName= "Ethan";
+// let accountBalance= 500000;
+// let eligible = `congratulations ${accountName} your loan has been disburbed`
+// let notEligible= "common getat"
+
+// if (accountBalance >= 500000) {
+//     console.log(eligible)
+    
+// }
+// else{
+//     console.log(notEligible)
+// }
+
+
+//function
+function addNumbers \'trbfe=-]['
+
+
+
+
+
+
